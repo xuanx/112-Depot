a90c11631e158f2be94bf0eb0aca8f061f092b3d
chaos <wuyao.chaos@gmail.com>
2012-07-10 10:00:30 +0800

    add: bookstore core

---
+++ bookstore/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bookstore::Application.config.secret_token = '23fd23481b77f10e4b7047795804d71d2d6f979f5c5c9ede956d278668f3ee252340cb312864b219342b471ed3590d910131eac1ce1974c367727881e325639e'
