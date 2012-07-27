42d295092af4ee7adf82824b7a2e7e7948bdd50b
prosper <prosper@ubuntu.ubuntu-domain>
2012-07-27 14:51:53 +0800

    renew the configuration letter

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,5 +1,5 @@
 class Notifier < ActionMailer::Base
-  default :from => "490778884@qq.com"
+  default :from => "depot@example.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
