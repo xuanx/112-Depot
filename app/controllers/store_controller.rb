9aa367bfa6f0c2a5f888792cc1006b5a2c980a77
John <mail2johnwong@gmail.com>
2012-08-01 15:27:16 +0800

    Update app/controllers/store_controller.rb
    if,else,end这种格式的代码对齐，对于整个文件的阅读将非常有帮助，有时候，尤其是end的遗漏，对于debug有很大影响

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,12 +2,12 @@ class StoreController < ApplicationController
   skip_before_filter :authorize
 
   def index
-		if params[:set_locale]
-		  redirect_to store_path(:locale => params[:set_locale])
-		else
-		@products = Product.all
-		@cart = current_cart
-		end
+  	if params[:set_locale]
+  	    redirect_to store_path(:locale => params[:set_locale])
+	else
+	    @products = Product.all
+   	    @cart = current_cart
+	end
   end
 
 end
