9f3a40ceabe6d836af873b27bf88f5feb613fb50
zhangjian <779325088@qq.com>
2012-08-08 11:50:15 +0800

    chinese language translation

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -1,8 +1,10 @@
 #encoding: utf-8
 
 I18n.default_locale = :en
+#LOCALES_DIRECTORY = "#{C:\Documents and Settings\user\My Documents\GitHub\}/112-Depot/config/locales"
 
 LANGUAGES = [
   ['English',                  'en'],
-  ["Espa&ntilde;ol".html_safe, 'es']
+  ["Espa&ntilde;ol".html_safe, 'es'],
+  ["Chinese".html_safe,        'zh']
 ]
